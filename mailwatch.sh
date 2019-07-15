0c7dfdf87b7e626c3ee26166003b27529902f2d7
Arie <38727432+T100D@users.noreply.github.com>
2019-07-15 11:43:12 +0200

    Handle spaces in filenames

--- mailwatch.sh
+++ mailwatch.sh
@@ -1,16 +1,28 @@
-    #!/bin/bash
-    #
-    # Door: Arie van den Heuvel
-    # Doel: Controleren van een map en het te aanwezige bestand verzenden
-    #
-    # 03-07-2019
-    # versie 0.1
-    #
+#!/bin/bash
+#
+# Door: Arie van den Heuvel
+# Purpose: Create dumpfolder te send a file by mail
+#
+# 03-07-2019
+# versie 0.1
+#
+# 12-07-2019
+# versie 0.2 including multiple file handling
+#
+# 15-07-2019
+# Versie 0.3 include handling spaces in flienames
+#
 
-    if  [ $(ls -1A /home/wesp/Mail/outgoing_mail/ | wc -l) -gt 0 ];
-         then file=$(ls -1A /home/wesp/Mail/outgoing_mail/) ;
-         echo "Wesp bestand - please do not replay to this email" | mutt -a /home/wesp/Mail/outgoing_mail/$file -s "Wesp filetransfer $file " -- (receiver addres) ;
-         mv /home/wesp/Mail/outgoing_mail/$file /home/wesp/Mail/send_mail/
-        else
-         exit
-    fi
+if  [ $(ls -1A /home/wesp/Mail/outgoing_mail/ | wc -l) -gt 0 ];
+    then
+        for file in /home/wesp/Mail/outgoing_mail/*
+        do
+        mv "$file" "${file// /_}" ;done
+        for sendfile in /home/wesp/Mail/outgoing_mail/*
+        do
+        echo "Wesp bestand - please do not replay to this email" | mutt -a $sendfile -s "Wesp filetransfer $sendfile" -- you@yourselv.com ;
+        mv $sendfile /home/wesp/Mail/send_mail/
+        done
+   else
+      exit
+fi
