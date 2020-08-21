36f1d489d75bfd7e7e054bc70f8196dc2c2d3692
Arie <38727432+T100D@users.noreply.github.com>
2020-08-21 13:36:35 +0200

    Minor update

--- startwespweb.sh
+++ startwespweb.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 #
-# The VNC server does not support OpenGL extensions. The --disable-webgl is to make sure Chrome doesn't try to use OpenGL/WebGL
+# The VNC server does not support OpenGL extensions.
+#
+# Disable annoying password store question --password-store=basic
 # The --disable-gpu is to disable the GPU error on starting
 # The --password-store=basic disables the keyring question at opening the browser
-# The  --new-window is needed for a sepperate window in combination with --user-data-dir=/home/wesp/.config/chromium/
+# The --new-window is needed for a sepperate window in combination with --user-data-dir=/home/wesp/.config/chromium/
 # this directory is the same as the basig datastor so the windows are linked
-# wmctrl positions the second window o the first screen
+#
+# wmctrl positions the second started window on the first screen
 #
 # Vesion 1.0 dd 24-02-2020
 #
-exec /usr/bin/chromium-browser --disable-webgl --disable-gpu --password-store=basic --window-size=1920,1166 --window-position=1921,28 %u http://our.applciation.url & sleep 1 ; /usr/bin/chromium-browser --disable-webgl --user-data-dir=/home/wesp/.config/chromium/ --disable-gpu --new-window --password-store=basic --window-position=1921,28 --window-size=1920,1166 %u http://our.applciation.url & sleep 1 ; wmctrl -r :ACTIVE: -e 0,0,28,1919,1166
+exec /usr/bin/chromium-browser --password-store=basic --window-size=1920,1166 --window-position=1921,28 %u http://our.applciation.url & sleep 1 ; /usr/bin/chromium-browser --user-data-dir=/home/wesp/.config/chromium/ --new-window --password-store=basic --window-position=1921,28 --window-size=1920,1166 %u http://our.applciation.url & sleep 1 ; wmctrl -r :ACTIVE: -e 0,0,28,1919,1166
