9b8f8fdc2c0da2c600d1134d1a96a6209246523d
Arie <38727432+T100D@users.noreply.github.com>
2022-02-07 15:39:08 +0100

    Update for changing to google-chrome

--- startwespweb.sh
+++ startwespweb.sh
@@ -11,6 +11,10 @@
 #
 # wmctrl positions the second started window on the first screen
 #
-# Vesion 1.1 dd 29-01-2021
+# Version 1.1 dd 29-01-2021
+# Version 2.0 dd 07-02-2022 after switching form chromium to chrome
 #
-exec /usr/bin/chromium-browser --password-store=basic --disable-gpu --no-xshm --window-size=1920,1166 --window-position=1921,28 %u http://our.applciation.url & sleep 1 ; /usr/bin/chromium-browser --user-data-dir=/home/wesp/.config/chromium/ --new-window --password-store=basic --disable-gpu --no-xshm --window-position=1921,28 --window-size=1920,1166 %u http://our.applciation.url & sleep 1 ; wmctrl -r :ACTIVE: -e 0,0,28,1919,1166
+# exec /usr/bin/chromium-browser --password-store=basic --disable-gpu --no-xshm --window-size=1920,1166 --window-position=1921,28 %u http://our.applciation.url & sleep 1 ; /usr/bin/chromium-browser --user-data-dir=/home/wesp/.config/chromium/ --new-window --password-store=basic --disable-gpu --no-xshm --window-position=1921,28 --window-size=1920,1166 %u http://our.applciation.url & sleep 1 ; wmctrl -r :ACTIVE: -e 0,0,28,1919,1166
+#
+# HVL
+/usr/bin/google-chrome-stable --password-store=basic --window-size=1920,1132 --window-position=1921,18 %u https://wesp.schelderadar.net/wesp/ & sleep 1 ; /usr/bin/google-chrome-stable --new-window --password-store=basic --window-size=1920,1132 --window-position=1921,18 %u https://wesp.schelderadar.net/wesp/ & sleep 1 ; wmctrl -r :ACTIVE: -e 0,-40,18,1962,1176
